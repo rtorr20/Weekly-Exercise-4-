fc40dbb6afc3ebbc8c067e4e9cd05a935c66d19f
Roxana Torres <rtorr20@uic.edu>
2025-09-20 15:13:25 -0500

    6.a

--- WEEKLYEXERCISE4.R
+++ WEEKLYEXERCISE4.R
@@ -1,1 +1,11 @@
 #[1] "main"
+
+      
+library(readr)
+library(dplyr)
+library(ggplot2)
+
+Olympics <- read_csv("Olympics.csv")
+
+oly <- Olympics %>%
+  mutate(total.medals = gold + silver + bronze)
