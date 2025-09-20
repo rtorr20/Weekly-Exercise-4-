ade55bb66561acd4f64bc30777586dc3dbccafec
Roxana Torres <rtorr20@uic.edu>
2025-09-20 15:56:48 -0500

    Q7

--- WEEKLYEXERCISE4.R
+++ WEEKLYEXERCISE4.R
@@ -25,3 +25,29 @@ print(largest1992)
 8 Czechoslovakia                     74
 9 Switzerland                        74
 10 Sweden                             73
+
+
+
+
+count_5 <- c("United States", "France", "Germany", "Russia", "China")
+
+gold_times <- olymp %>%
+  filter(country %in% count_5) %>%
+  mutate(year = as.integer(year)) %>%
+  group_by(country, year) %>%
+  summarise(gold = sum(gold, na.rm = TRUE), .groups = "drop") %>%
+  arrange(country, year)
+
+
+
+
+ggplot(gold_times, aes(x = year, y = gold, color = country, group = country)) +
+  geom_line(linewidth = 0.9) +
+  geom_point(size = 2) +
+  labs(
+    title = "Gold Medals Over Time (Selected Countries)",
+    x = "Year",
+    y = "Gold Medals")
+
+
+
