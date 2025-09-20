b626d03420074f00476d74a7d851f9db0f64bd3a
Roxana Torres <rtorr20@uic.edu>
2025-09-20 15:19:30 -0500

    6.b

--- WEEKLYEXERCISE4.R
+++ WEEKLYEXERCISE4.R
@@ -9,3 +9,26 @@ Olympics <- read_csv("Olympics.csv")
 
 oly <- Olympics %>%
   mutate(total.medals = gold + silver + bronze)
+
+
+bycountry <- oly %>%
+  group_by(country) %>%
+  summarise(total_gold = sum(gold), na.rm = TRUE) %>%
+  arrange(desc(total_gold))
+
+head(bycountry, 10)
+
+
+#A tibble: 10 × 3
+#country       total_gold na.rm
+
+1 Germany               72  
+2 Norway                68 
+3 United States         66 
+4 Canada                51 
+5 Russia                49  
+6 Austria               37 
+7 Switzerland           34 
+8 Netherlands           28 
+9 Sweden                28 
+10 East Germany          27 
